7d4e68b22d021864dc87b19b0a8dea2c1ce086a5
Danielly Khalil Cândido Teixeira <152915978+Khalielly@users.noreply.github.com>
2024-04-25 21:48:38 -0300

    Update script.js

--- PROJETO PRÁTICO/FRAMEWORK_ANGULAR/script.js
+++ PROJETO PRÁTICO/FRAMEWORK_ANGULAR/script.js
@@ -1,9 +1,9 @@
 const modal = document.querySelector('.modal-container')
 const tbody = document.querySelector('tbody')
 const sNome = document.querySelector('#m-nome')
-const sFuncao = document.querySelector('m-funcao')
+const sFuncao = document.querySelector('#m-funcao')
 const sSalario = document.querySelector('#m-salario')
-const btnSalvar = document.querySelector('btnSalvar')
+const btnSalvar = document.querySelector('#btnSalvar')
 
 let itens
 let id
@@ -49,11 +49,44 @@ function insertItem(item, index) {
         <td>${item.funcao}</td>
         <td>R$ ${item.salario}</td>
         <td class="acao">
-            <button onclick="editItem(${index})"<i class='bx bx-edit'></i></button>
+            <button onclick="editItem(${index})"><i class='bx bx-edit'></i></button>
         </td>
         <td class="acao">
-            <button onclick="deleteItem(${index})"<i class='bx bx-trash'></i></button>
+            <button onclick="deleteItem(${index})"><i class='bx bx-trash'></i></button>
         </td>
     `
     tbody.appendChild(tr)
 }
+btnSalvar.onclick = e => {
+    if (sNome.value == '' || sFuncao.value == '' || sSalario.value == ''){
+        return
+    }
+    e.preventDefault();
+
+    if (id !== undefined) {
+        itens[id].nome = sNome.value
+        itens[id].funcao = sFuncao.value
+        itens[id].salario = sSalario.value
+    } else {
+        itens.push({'nome': sNome.value, 'funcao': sFuncao.value, 'salario': sSalario.value})
+    }
+
+    setItensBD()
+
+    modal.classList.remove('active')
+    loadItens()
+    id = undefined
+}
+
+function loadItens() {
+    itens = getItensBD()
+    tbody.innerHTML = ''
+    itens.forEach((item, index) => {
+        insertItem(item, index)
+    })
+}
+
+const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? []
+const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens))
+
+loadItens()
