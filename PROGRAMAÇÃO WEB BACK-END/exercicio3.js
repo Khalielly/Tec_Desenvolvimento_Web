09b667a4780cb54dae9d66c58edf3a34ef3860fd
Khalielly <daniellycandido45@gmail.com>
2024-04-29 20:44:32 -0300

    resposta_atividade3

---
+++ PROGRAMAÇÃO WEB BACK-END/exercicio3.js
@@ -0,0 +1,41 @@
+//---------------------resposta1---------------------
+
+// let soma = 0, i = 1;
+
+// while(i <= 100){
+//     soma+=i;
+//     i++
+// }
+//console.log(soma);
+
+//---------------------resposta2---------------------
+
+// let soma2 = 0;
+
+// for (let j = 1; j <= 100; j++){
+//     soma2+=j;
+// }
+// console.log(soma2);
+
+//---------------------resposta3---------------------
+
+// let soma3 = 0;
+// const lista = [];
+
+// for (let i=0; i<=100; i++){
+//     lista.push(i);
+// }
+
+// for (i in lista){
+//     soma3 += parseInt(i);
+//     console.log(soma3);
+// }
+
+//---------------------resposta4---------------------
+
+// let soma4 = 0;
+// function somar(i){
+//     soma4 += parseInt(i);
+// }
+// lista.forEach(somar);
+// console.log(soma4);
