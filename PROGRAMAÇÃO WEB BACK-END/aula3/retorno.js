a649ef18762dac7ecddd7a2ac8d9c2fc6d6ec706
Danielly Khalil Cândido Teixeira <152915978+Khalielly@users.noreply.github.com>
2024-05-03 20:01:14 -0300

    Add files via upload

---
+++ PROGRAMAÇÃO WEB BACK-END/aula3/retorno.js
@@ -0,0 +1,11 @@
+function test(lista) {
+    let soma = 0;
+    // for (let i = 0; i < lista.length; i++) {
+    //     soma += lista[i];
+    // }
+    // return soma;
+    
+}
+
+let minhas_lista = test([0, 3, 5, 9, 5]);
+console.log(minhas_lista)
