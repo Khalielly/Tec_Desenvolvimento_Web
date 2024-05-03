8eea380dbe9f3c5480be6142d9a16694880d8403
Danielly Khalil Cândido Teixeira <152915978+Khalielly@users.noreply.github.com>
2024-05-03 20:25:23 -0300

    Update retorno.js

--- PROGRAMAÇÃO WEB BACK-END/aula3/retorno.js
+++ PROGRAMAÇÃO WEB BACK-END/aula3/retorno.js
@@ -1,11 +1,19 @@
 function test(lista) {
     let soma = 0;
-    // for (let i = 0; i < lista.length; i++) {
-    //     soma += lista[i];
+    for (let i = 0; i < lista.length; i++) {
+        soma += lista[i];
+    }
+    return soma;
+
+    //-------------------------- resposta 2 utilizando parseInt ----------------------------------
+
+    // for (elemento in lista) {
+    //     soma += parseInt(lista[elemento]);
     // }
+
     // return soma;
-    
+
 }
 
-let minhas_lista = test([0, 3, 5, 9, 5]);
+let minhas_lista = test([5, 3]);
 console.log(minhas_lista)
