a7034f542cea4f4401c438e0c46ea3a2c02c2946
Danielly Khalil Cândido Teixeira <152915978+Khalielly@users.noreply.github.com>
2024-05-03 21:31:10 -0300

    Add files via upload

---
+++ PROGRAMAÇÃO WEB BACK-END/aula3/exercicio_1.js
@@ -0,0 +1,8 @@
+let n1 = 2;
+const pi = Math.PI;
+
+let area = (2 ** 2) * pi;
+
+area = area.toFixed(2);
+
+console.log(area);
---
+++ PROGRAMAÇÃO WEB BACK-END/aula3/exercicio_2.js
@@ -0,0 +1,9 @@
+function calculo(data_niver){
+    const hoje=  new Date();
+    console.log(hoje.getDate())
+    console.log(hoje.getMonth())
+    console.log(hoje.getFullYear())
+}
+
+let data_aniver = "14/08/2005"
+let idade = data_aniver(data_aniver)
