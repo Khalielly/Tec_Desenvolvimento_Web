7c3cc6916adf2380c920d116bd6be3c22caacb7a
Danielly Khalil Cândido Teixeira <152915978+Khalielly@users.noreply.github.com>
2024-05-11 14:03:20 -0300

    up

--- PROGRAMAÇÃO WEB BACK-END/aula5/servidor.js
+++ PROGRAMAÇÃO WEB BACK-END/aula5/servidor.js
@@ -1,13 +1,28 @@
 const http = require('http');
 
 const port = 3000;
+const host = "localhost";
+const botao = 'console.log("to no front")';
+const resposta = `
+<head>
+    <title>Exemplo Client-Side</title>
+</head>
+<body>
+    <button id="meuBotao">Clique aqui</button>
+    <script>${botao}</script>
+</body>
+</html>`
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Olá Mundo!\n');
-});
+const resquestListener = function (req, res) {
+    
+    console.log("Chegou uma requisicao");
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    res.end(resposta);
+}
 
-server.listen(port, () =>{
-    console.log(`Servidor rodando em http://localhost:${port}`);
+const server = http.createServer(resquestListener);
+
+server.listen(port, host, () =>{
+    console.log(`Servidor rodando em http://${host}:${port}/`);
 
 });
