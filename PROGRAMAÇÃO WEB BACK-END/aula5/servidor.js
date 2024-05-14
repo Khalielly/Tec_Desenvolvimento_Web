980d76a60d2005c13e39e5872250554cf5d35daf
Danielly Khalil Cândido Teixeira <152915978+Khalielly@users.noreply.github.com>
2024-05-13 21:39:09 -0300

    up

--- PROGRAMAÇÃO WEB BACK-END/aula5/servidor.js
+++ PROGRAMAÇÃO WEB BACK-END/aula5/servidor.js
@@ -1,28 +1,28 @@
 const http = require('http');
 
 const port = 3000;
-const host = "localhost";
-const botao = 'console.log("to no front")';
-const resposta = `
-<head>
-    <title>Exemplo Client-Side</title>
-</head>
-<body>
-    <button id="meuBotao">Clique aqui</button>
-    <script>${botao}</script>
-</body>
-</html>`
+const host = 'localhost';
+const resposta = `Minha Pagina`;
 
 const resquestListener = function (req, res) {
-    
-    console.log("Chegou uma requisicao");
-    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    const { method, url } = req;
+
+    if (url == '/teste') {
+        res.end("Isso é um teste");
+    } else if (url == '/professorteste') {
+        res.end('Este é um prof');
+    } else if (url == '/alunoresposta') {
+        res.end('Isto é um aluno');
+    }
+
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
     res.end(resposta);
 }
 
-const server = http.createServer(resquestListener);
+const server = http.createServer();
+server.on('request', resquestListener);
 
-server.listen(port, host, () =>{
+server.listen(port, host, () => {
     console.log(`Servidor rodando em http://${host}:${port}/`);
 
 });
